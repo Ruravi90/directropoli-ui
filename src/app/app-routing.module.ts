1d217e5b9a94e0cac669acb7a4f6c6bcc2b77c3d
Ruravi Aguilar <eaguilar.arrezola@gmail.com>
2021-08-10 20:28:44 -0500

    Init git
    Inicia del directropoli-ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent  } from './pages/signin/signin.component';
+import { RegisterComponent  } from './pages/register/register.component';
+import { LandingComponent  } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/landing' },
+  {
+    path: 'signin', component: SigninComponent,
+  },
+  {
+    path: 'register', component: RegisterComponent,
+  },
+  {
+    path: 'landing', component: LandingComponent,
+  },
+  {
+    path: 'dashboard', loadChildren: () => import('./layout/main.module').then(m => m.MainModule)
+  },
+  { path: '**', component: LandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layout/main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MainRoutingModule } from './routing.module';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  imports: [
+    MainRoutingModule
+  ],
+  declarations: [MainComponent],
+  exports: [MainComponent]
+})
+export class MainModule {
+ }
